bf49730b2e11d5a3fd5171b8afe8100ea37dd281
Alec Gorge <alecgorge@gmail.com>
2013-09-22 20:44:10 -0700

    Initial Commit

---
+++ Listen to the Dead/GDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GDAppDelegate.h
+//  Listen to the Dead
+//
+//  Created by Alec Gorge on 9/22/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Listen to the Dead/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Listen to the Dead
+//
+//  Created by Alec Gorge on 9/22/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([GDAppDelegate class]));
+	}
+}
